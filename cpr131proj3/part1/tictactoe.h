1185db509a07d1ce5b72c8745bc4f5e2290cee8f
Long Duong <longduonglq@gmail.com>
2021-03-02 14:08:39 -0800

    qqt

--- cpr131proj3/part1/tictactoe.h
+++ cpr131proj3/part1/tictactoe.h
@@ -1,1 +1,32 @@
 #pragma once
+
+class TicTacToe
+{
+public:
+	enum Verdict {
+		USER_WINS,
+		USER_LOSES,
+		NEITHER
+	};
+	// init gameBoard to 0
+	TicTacToe();
+	
+	// run game loop
+	void run();
+
+	// set all cells in board to value
+	void setBoardTo(bool value);
+
+	// write AI move to board
+	void makeAIMove();
+
+	// write board on screen, indented by space
+	void drawBoardOnScreen(int space);
+
+	// outputs whether user wins, loses, or neither is true
+	Verdict checkVerdict();
+
+private:
+	bool gameBoard[3][3];
+
+};
