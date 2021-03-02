2c2c11a604d07977c9784a2a852d846478cb349d
Christopher <programmerchristopher@gmail.com>
2021-03-01 20:31:52 -0800

    timer

--- Timer.cpp
+++ Timer.cpp
@@ -3,16 +3,22 @@
 // Description: 
 
 #include <time.h>
+#include <vector>
 #include "Timer.h"
 
 Timer::Timer() {
 	start();
 }
-
 Timer::start() {
 	start_time = time;
 }
-
 Timer::end() {
 	end_time = time;
+	//this_time = end_time - start_time;
+}
+Timer::getSeconds() {
+	//return ;
+}
+Timer::getMinutes() {
+	//floor 
 }
--- Timer.h
+++ Timer.h
@@ -3,6 +3,7 @@
 // Description: 
 
 #pragma once
+#include <vector>
 
 class Timer {
 	public:
@@ -13,8 +14,18 @@ class Timer {
 		// Precondition: 
 		// Postcondition: 
 		void start();
+		// Precondition: 
+		// Postcondition: 
 		void end();
 
+		// Precondition: 
+		// Postcondition: 
+		int getSeconds();
+		// Precondition: 
+		// Postcondition: 
+		int getMinutes();
+
 	private:
-		long long start_time, end_time;
+		long long start_time, end_time, this_time;
+
 };
