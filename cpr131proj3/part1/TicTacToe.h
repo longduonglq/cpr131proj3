88f80640b70fd1f1c5cde09c8b0954acf3ca3156
Long Duong <longduonglq@gmail.com>
2021-03-07 23:33:51 -0800

    with ai

--- cpr131proj3/part1/TicTacToe.h
+++ cpr131proj3/part1/TicTacToe.h
@@ -1,19 +1,24 @@
 #pragma once
+
+#include "ai.h"
+
 class TicTacToe
 {
-	private:
-		char boardSlots[3][3];
-		char computer;
-		char player;
+private:
+	char boardSlots[3][3];
+	char computer;
+	char player;
 
-	public: 
-		TicTacToe();						//default constructor
-		//void initBoard();					//Initate the board only to show the numbers
-		void displayBoard();				//Update after each move
-		void resetBoard();					//Reset the board
-		char checkBoard();				    //Check for possible win/lose/draw 
-		void compMove();				    //Computer make a move
-		void playerMove(int row, int col);  //User make a move
+public:
+	TicTacToe();						//default constructor
+	void run();
+	//void initBoard();					//Initate the board only to show the numbers
+	void displayBoard();				//Update after each move
+	void resetBoard();					//Reset the board
+	void checkBoard();				    //Check for possible win/lose/draw
+	void compMove();				    //Computer make a move
+	void playerMove(int row, int col);  //User make a move
 
 };
 
+void tictactoeStarter();
