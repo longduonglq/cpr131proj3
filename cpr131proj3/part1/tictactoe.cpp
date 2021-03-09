f4531d0a5541cfa69788518fbcc4d9148ada400c
Christopher <programmerchristopher@gmail.com>
2021-03-09 13:08:56 -0800

    tic tac toe

--- cpr131proj3/part1/tictactoe.cpp
+++ cpr131proj3/part1/tictactoe.cpp
@@ -2,14 +2,11 @@
 #include <iostream>
 #include "input.h"
 #include "ai.h"
-#include "Timer.h"
+#include "../timer/Timer.h"
 using namespace std;
 
 TicTacToe::TicTacToe()
 {
-	Timer myTimer;                                      //create class
-	myTimer.start();                                //start timer
-
 	computer = 'O';
 	player = 'X';
 	gameStat = 'U';			//U for unfinished
@@ -27,18 +24,25 @@ TicTacToe::TicTacToe()
 
 void TicTacToe::playerMove(int row, int col)
 {
+	if (boardSlots[row - 1][col - 1] != ' ')
+	{
+		cout << "Position is already occupied" << endl;
+		return;
+	}
 	boardSlots[row - 1][col - 1] = 'X';
 	moveCounter++;
 	cout << "move counter is: " << moveCounter << endl;
 }
 
-void TicTacToe::compMove(int row, int col) //This is player2, for testing purposes
+void TicTacToe::compMove() //This is player2, for testing purposes
 {
-	boardSlots[row - 1][col - 1] = 'O';
-	moveCounter++;
-	cout << "move counter is: " << moveCounter << endl;
+	AI ai(boardSlots);
+	auto bestMove = ai.getBestMove();
+	if (bestMove.first < 0 || bestMove.second < 0)
+		return;
+	boardSlots[bestMove.first][bestMove.second] = computer;
 }
- 
+
 void TicTacToe::displayBoard()
 {
 	// Upper outline
@@ -78,128 +82,246 @@ bool TicTacToe::didComputerWin()
 {
 	return AI(boardSlots).didSelfWin();
 }
+//bool TicTacToe::isTie()
+//{
+//	return false;
+//}
+
+//char TicTacToe::checkBoard()  //Check for possible win/lose/draw
+//{
+//	//checking XXX for each rows
+//	if (boardSlots[0][0] == 'X' && boardSlots[0][1] == 'X' && boardSlots[0][2] == 'X')
+//		return 'X';
+//	else if (boardSlots[1][0] == 'X' && boardSlots[1][1] == 'X' && boardSlots[1][2] == 'X')
+//		return 'X';
+//	else if (boardSlots[2][0] == 'X' && boardSlots[2][1] == 'X' && boardSlots[2][2] == 'X')
+//		return 'X';
+//
+//	//checking XXX for each col
+//	else if (boardSlots[0][0] == 'X' && boardSlots[1][0] == 'X' && boardSlots[2][0] == 'X')
+//		return 'X';
+//	else if (boardSlots[0][1] == 'X' && boardSlots[1][1] == 'X' && boardSlots[2][1] == 'X')
+//		return 'X';
+//	else if (boardSlots[0][2] == 'X' && boardSlots[1][2] == 'X' && boardSlots[2][2] == 'X')
+//		return 'X';
+//
+//	//checking XXX for the diagonals
+//	else if (boardSlots[0][0] == 'X' && boardSlots[1][1] == 'X' && boardSlots[2][2] == 'X')
+//		return 'X';
+//	else if (boardSlots[0][2] == 'X' && boardSlots[1][1] == 'X' && boardSlots[2][0] == 'X')
+//		return 'X';
+//
+//	//-----------------------------------------------------------------------------------//
+//
+//	//checking OOO for each rows
+//	else if (boardSlots[0][0] == 'O' && boardSlots[0][1] == 'O' && boardSlots[0][2] == 'O')
+//		return 'O';
+//	else if (boardSlots[1][0] == 'O' && boardSlots[1][1] == 'O' && boardSlots[1][2] == 'O')
+//		return 'O';
+//	else if (boardSlots[2][0] == 'O' && boardSlots[2][1] == 'O' && boardSlots[2][2] == 'O')
+//		return 'O';
+//
+//	//checking OOO for each col
+//	else if (boardSlots[0][0] == 'O' && boardSlots[1][0] == 'O' && boardSlots[2][0] == 'O')
+//		return 'O';
+//	else if (boardSlots[0][1] == 'O' && boardSlots[1][1] == 'O' && boardSlots[2][1] == 'O')
+//		return 'O';
+//	else if (boardSlots[0][2] == 'O' && boardSlots[1][2] == 'O' && boardSlots[2][2] == 'O')
+//		return 'O';
+//
+//	//checking OOO for the diagonals
+//	else if (boardSlots[0][0] == 'O' && boardSlots[1][1] == 'O' && boardSlots[2][2] == 'O')
+//		return 'O';
+//	else if (boardSlots[0][2] == 'O' && boardSlots[1][1] == 'O' && boardSlots[2][0] == 'O')
+//		return 'O';
+//
+//	//No Wins
+//	else if (moveCounter == 8)
+//		return 'D';
+//
+//	// Game is unfinished
+//	else
+//		return 'U';
+//}
+
+//bool TicTacToe::didUserWin()
+//{
+//	//checking XXX for each rows
+//	if (boardSlots[0][0] == 'X' && boardSlots[0][1] == 'X' && boardSlots[0][2] == 'X')
+//		return true;
+//	else if (boardSlots[1][0] == 'X' && boardSlots[1][1] == 'X' && boardSlots[1][2] == 'X')
+//		return true;
+//	else if (boardSlots[2][0] == 'X' && boardSlots[2][1] == 'X' && boardSlots[2][2] == 'X')
+//		return true;
+//
+//	//checking XXX for each col
+//	else if (boardSlots[0][0] == 'X' && boardSlots[1][0] == 'X' && boardSlots[2][0] == 'X')
+//		return true;
+//	else if (boardSlots[0][1] == 'X' && boardSlots[1][1] == 'X' && boardSlots[2][1] == 'X')
+//		return true;
+//	else if (boardSlots[0][2] == 'X' && boardSlots[1][2] == 'X' && boardSlots[2][2] == 'X')
+//		return true;
+//
+//	//checking XXX for the diagonals
+//	else if (boardSlots[0][0] == 'X' && boardSlots[1][1] == 'X' && boardSlots[2][2] == 'X')
+//		return true;
+//	else if (boardSlots[0][2] == 'X' && boardSlots[1][1] == 'X' && boardSlots[2][0] == 'X')
+//		return true;
+//
+//	else
+//		return false;
+//}
+//
+//bool TicTacToe::didComputerWin()
+//{
+//	//checking OOO for each rows
+//	if (boardSlots[0][0] == 'O' && boardSlots[0][1] == 'O' && boardSlots[0][2] == 'O')
+//	return true;
+//	else if (boardSlots[1][0] == 'O' && boardSlots[1][1] == 'O' && boardSlots[1][2] == 'O')
+//	return true;
+//	else if (boardSlots[2][0] == 'O' && boardSlots[2][1] == 'O' && boardSlots[2][2] == 'O')
+//	return true;
+//
+//	//checking OOO for each col
+//	else if (boardSlots[0][0] == 'O' && boardSlots[1][0] == 'O' && boardSlots[2][0] == 'O')
+//	return true;
+//	else if (boardSlots[0][1] == 'O' && boardSlots[1][1] == 'O' && boardSlots[2][1] == 'O')
+//	return true;
+//	else if (boardSlots[0][2] == 'O' && boardSlots[1][2] == 'O' && boardSlots[2][2] == 'O')
+//	return true;
+//
+//	//checking OOO for the diagonals
+//	else if (boardSlots[0][0] == 'O' && boardSlots[1][1] == 'O' && boardSlots[2][2] == 'O')
+//	return true;
+//	else if (boardSlots[0][2] == 'O' && boardSlots[1][1] == 'O' && boardSlots[2][0] == 'O')
+//	return true;
+//
+//	else
+//	return false;
+//}
+//
 bool TicTacToe::isTie()
 {
-	return false;
-}
-
-char TicTacToe::checkBoard()  //Check for possible win/lose/draw
-{
-	//checking XXX for each rows
-	if (boardSlots[0][0] == 'X' && boardSlots[0][1] == 'X' && boardSlots[0][2] == 'X')
-		return 'X';
-	else if (boardSlots[1][0] == 'X' && boardSlots[1][1] == 'X' && boardSlots[1][2] == 'X')
-		return 'X';
-	else if (boardSlots[2][0] == 'X' && boardSlots[2][1] == 'X' && boardSlots[2][2] == 'X')
-		return 'X';
-
-	//checking XXX for each col
-	else if (boardSlots[0][0] == 'X' && boardSlots[1][0] == 'X' && boardSlots[2][0] == 'X')
-		return 'X';
-	else if (boardSlots[0][1] == 'X' && boardSlots[1][1] == 'X' && boardSlots[2][1] == 'X')
-		return 'X';
-	else if (boardSlots[0][2] == 'X' && boardSlots[1][2] == 'X' && boardSlots[2][2] == 'X')
-		return 'X';
-
-	//checking XXX for the diagonals
-	else if (boardSlots[0][0] == 'X' && boardSlots[1][1] == 'X' && boardSlots[2][2] == 'X')
-		return 'X';
-	else if (boardSlots[0][2] == 'X' && boardSlots[1][1] == 'X' && boardSlots[2][0] == 'X')
-		return 'X';
-
-	//-----------------------------------------------------------------------------------//
-
-	//checking OOO for each rows
-	else if (boardSlots[0][0] == 'O' && boardSlots[0][1] == 'O' && boardSlots[0][2] == 'O')
-		return 'O';
-	else if (boardSlots[1][0] == 'O' && boardSlots[1][1] == 'O' && boardSlots[1][2] == 'O')
-		return 'O';
-	else if (boardSlots[2][0] == 'O' && boardSlots[2][1] == 'O' && boardSlots[2][2] == 'O')
-		return 'O';
-
-	//checking OOO for each col
-	else if (boardSlots[0][0] == 'O' && boardSlots[1][0] == 'O' && boardSlots[2][0] == 'O')
-		return 'O';
-	else if (boardSlots[0][1] == 'O' && boardSlots[1][1] == 'O' && boardSlots[2][1] == 'O')
-		return 'O';
-	else if (boardSlots[0][2] == 'O' && boardSlots[1][2] == 'O' && boardSlots[2][2] == 'O')
-		return 'O';
-
-	//checking OOO for the diagonals
-	else if (boardSlots[0][0] == 'O' && boardSlots[1][1] == 'O' && boardSlots[2][2] == 'O')
-		return 'O';
-	else if (boardSlots[0][2] == 'O' && boardSlots[1][1] == 'O' && boardSlots[2][0] == 'O')
-		return 'O';
-
-	//No Wins
-	else if (moveCounter == 8)
-		return 'D';
-
-	// Game is unfinished
+	if (moveCounter == 9)
+		return true;
 	else
-		return 'U';
+		return false;
 }
 
-char TicTacToe::gameStatus()
+
+void tictactoeStarter()
 {
+	int row;
+	int col;
+
+	int row1; //for player 2 
+	int col1;
+
 	char playGame; //Continue play again yes or no
 	char yes = 'y', no = 'n';
+	TicTacToe test1;
 
-	if (checkBoard() == 'X')			//<----This is where you check for winners/loser/draw
+	cout << "Tic-Tac-Toe" << endl;
+	test1.displayBoard();
+
+
+	do
 	{
-		displayBoard();
-		cout << "Player wins!" << endl;
-		playGame = toupper(inputChar("Play again? (Y-yes or N-no): ", yes, no));
-		if (playGame == 'Y')
+		cout << "Enter the board's row # (1..3) or 0 to forfeit: " << endl;
+		cin >> row;
+		if (row == 0)
 		{
-			resetBoard();
-			displayBoard();
+			break;
 		}
-		else
+
+		cout << "Enter the board's column # (1..3) or 0 to forfeit: " << endl;
+		cin >> col;
+		if (col == 0)
 		{
-			//break;
-			return 'u';
+			break;
 		}
-	}
-	else if (checkBoard() == 'O')
-	{
-		displayBoard();
-		cout << "Computer wins!" << endl;
-		cout << "Play again? (Y-yes or N-no)" << endl;
-		cin >> playGame;
-		if (playGame == 'y')
+
+		test1.playerMove(row, col);				//<----this is where the player will input into the 2d array 
+		test1.displayBoard();
+		if (test1.didUserWin())
 		{
-			resetBoard();
-			displayBoard();
+			cout << "User Wins" << endl;
 		}
-		else
-		{
-			//break;
-			return 'u';
-		}
-	}
-	else if (checkBoard() == 'D')
-	{
-		displayBoard();
-		cout << "It is a Draw!" << endl;
-		cout << "Play again? (Y-yes or N-no)" << endl;
-		cin >> playGame;
-		if (playGame == 'Y')
-		{
-			resetBoard();
-			displayBoard();
-		}
-		else
-		{
-			//break;
-			return 'u';
-		}
-	}
-	else
-	{
-		displayBoard();
-	}
+
+		test1.compMove();
+		cout << "Computer moved!" << endl;
+		test1.displayBoard();
+
+
+		//---------------------------------------------------------------------------------------// Breakpoint
+		//This line is where the computer should make a move
+
+	} while (true);
+
+	return;
 }
+
+
+//char TicTacToe::gameStatus()
+//{
+//	char playGame; //Continue play again yes or no
+//	char yes = 'y', no = 'n';
+//
+//	if (checkBoard() == 'X')			//<----This is where you check for winners/loser/draw
+//	{
+//		displayBoard();
+//		cout << "Player wins!" << endl;
+//		playGame = toupper(inputChar("Play again? (Y-yes or N-no): ", yes, no));
+//		if (playGame == 'Y')
+//		{
+//			resetBoard();
+//			displayBoard();
+//		}
+//		else
+//		{
+//			//break;
+//			return 'u';
+//		}
+//	}
+//	else if (checkBoard() == 'O')
+//	{
+//		displayBoard();
+//		cout << "Computer wins!" << endl;
+//		cout << "Play again? (Y-yes or N-no)" << endl;
+//		cin >> playGame;
+//		if (playGame == 'y')
+//		{
+//			resetBoard();
+//			displayBoard();
+//		}
+//		else
+//		{
+//			//break;
+//			return 'u';
+//		}
+//	}
+//	else if (checkBoard() == 'D')
+//	{
+//		displayBoard();
+//		cout << "It is a Draw!" << endl;
+//		cout << "Play again? (Y-yes or N-no)" << endl;
+//		cin >> playGame;
+//		if (playGame == 'Y')
+//		{
+//			resetBoard();
+//			displayBoard();
+//		}
+//		else
+//		{
+//			//break;
+//			return 'u';
+//		}
+//	}
+//	else
+//	{
+//		displayBoard();
+//	}
+//}
 
 void endSession() {
 	myTimer.end();                                  //end timer
